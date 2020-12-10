3859ef60a2c79c2ae98b444e0a42b28bebd20972
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-12-10 08:40:54 -0500

    add emty page components

--- src/router.js
+++ src/router.js
@@ -3,17 +3,24 @@ import {
     createWebHistory
 } from 'vue-router';
 
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestRecived from './pages/requests/RequestRecived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path:'/', redirect:'/coaches'},
-        {path:'/coaches', component: null},
-        {path:'/coaches/:id', component: null, children: [
-            {path: 'contact', component:null}
+        {path:'/coaches', component: CoachList},
+        {path:'/coaches/:id', component: CoachDetail, children: [
+            {path: 'contact', component:ContactCoach}
         ]},
-        {path:'/register', component: null},
-        {path:'/requests', component: null},
-        {path:'/:notFound(.*)', component: null},
+        {path:'/register', component: CoachRegistration},
+        {path:'/requests', component: RequestRecived},
+        {path:'/:notFound(.*)', component: NotFound},
     ],
 });
 
