ef1c6b6498f58dc043bb95c11782d356642cacc1
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-12-11 00:13:57 -0500

    coahes basic store

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex';
+
+import coachModules from './modules/coaches/index.js';
+
+const store = createStore({
+    modules: {
+        coaches: coachModules
+    }
+});
+
+export default store;
