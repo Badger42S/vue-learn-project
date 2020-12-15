c03dd6f3de186c16805375fc9e50c63a84e8c5d9
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-12-15 11:45:54 -0500

    using requestItem

--- src/pages/requests/RequestRecived.vue
+++ src/pages/requests/RequestRecived.vue
@@ -1,3 +1,50 @@
 <template>
-    Request
+    <section>
+        <base-card>
+            <h2>Request recived</h2>
+            <ul v-if="hasRequest">
+                <request-item 
+                    v-for="request in recivedRequest" 
+                    :key="request.id"
+                    :email="request.userEmail"
+                    :message="request.message"
+                ></request-item>
+            </ul>
+            <h3 v-else>You haven't any request</h3>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import RequestItem from '../../components/requests/RequesItem.vue'
+export default {
+    components: {
+        RequestItem
+    },
+    computed:{
+        recivedRequest() {
+            return this.$store.getters['reuest/requests'];
+        },
+        hasRequest() {
+            return this.$store.getters['reuest/hasRequest'];
+        }
+    }
+}
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
