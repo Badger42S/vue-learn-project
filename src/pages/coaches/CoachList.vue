7638b7ce2f9ffc47fb5ed047f44e4cc00bde694a
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-12-19 07:54:14 -0500

    using loading coaches action

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -5,7 +5,7 @@
     <section>
         <base-card>
             <div class="controls">
-                <base-button mode="outline">Refresh</base-button>
+                <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
                 <base-button link to="/register" v-if="!isCoach">Register as coach</base-button>
             </div>
             <ul v-if="hasCoaches">
@@ -63,7 +63,13 @@ export default {
     methods: {
         setFilters(updatedFilters){
             this.activeFilters =updatedFilters;
+        },
+        loadCoaches() {
+            this.$store.dispatch('coaches/loadCoahes');
         }
+    },
+    created() {
+        this.loadCoaches();
     }
 }
 </script>
