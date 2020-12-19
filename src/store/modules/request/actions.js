667b293ec5d2c4012f6c6127c0a7d662ff91b270
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-12-19 11:59:35 -0500

    fetch requests

--- src/store/modules/request/actions.js
+++ src/store/modules/request/actions.js
@@ -1,12 +1,45 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
-            id: new Date().toISOString(),
-            coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
         };
 
+        const response = await fetch(`https://vue-coacj-project-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body:Json.stringify(newRequest)
+        });
+        const responseData = await response.json();
+        if(!response.ok) {
+            const err = new Error(response.message || 'Faild to send request.');
+            throw(err);
+        }
+        newRequest.id =responseData.name;
+        newRequest.coachId = payload.coachId;
+
         context.commit('addRequest', newRequest);
+    },
+    async fetshRquests(context) {
+        const coachId = context.rootGetters.userId;
+        const response = await fetch(`https://vue-coacj-project-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+        const responseData = await response.json();
+        if(!response.ok) {
+            const err = new Error(response.message || 'Faild to fetch requests.');
+            throw(err);
+        }
+
+        const requests =[];
+        for (const key in responseData) {
+            const request ={
+                id:key,
+                coachId: coachId,
+                userEmail:responseData[key].userEmail,
+                description: responseData[key].message
+            }
+
+            requests.push(request);
+        }
+
+        context.commit('setRequests', requests);
     }
 }
