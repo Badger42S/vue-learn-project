3017d4921f12150af1510efca2fe0df038011924
Badger42S <57646489+Badger42S@users.noreply.github.com>
2020-12-20 15:18:12 -0500

    not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <router-link to="/coaches">return</router-link>
+        </base-card>
+    </section>
+</template>
