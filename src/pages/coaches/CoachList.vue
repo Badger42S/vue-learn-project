057c6d111775570ae70484f8ea06426bedb2bb6b
Badger42S <57646489+Badger42S@users.noreply.github.com>
2021-01-05 08:39:33 -0500

    add checking authentication

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -10,7 +10,8 @@
             <base-card>
                 <div class="controls">
                     <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-                    <base-button link to="/register" v-if="!isCoach && !isLoading">Register as coach</base-button>
+                    <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+                    <base-button link to="/register" v-if="isLoggedIn && !isCoach && !isLoading">Register as coach</base-button>
                 </div>
                 <div v-if="isLoading">
                     <base-spiner></base-spiner>
@@ -70,6 +71,9 @@ export default {
         },
         isCoach() {
             return this.$store.getters['coaches/isCoach'];
+        },
+        isLoggedIn() {
+            return this.$store.getters.isAuthenticated;
         }
     },
     methods: {
