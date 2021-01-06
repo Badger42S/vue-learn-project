fecaedeb05fb7a8ff9e1ee50c2bfc91850d7f7d2
Badger42S <57646489+Badger42S@users.noreply.github.com>
2021-01-06 07:37:57 -0500

    global guard

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestRecived from './pages/requests/RequestRecived.vue';
 import AuthUsers from './pages/auth/AuthUsers.vue';
 import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
 
 const router = createRouter({
     history: createWebHashHistory(),
@@ -19,11 +20,21 @@ const router = createRouter({
         {path:'/coaches/:id', props:true,component: CoachDetail, children: [
             {path: 'contact', component:ContactCoach}
         ]},
-        {path:'/register', component: CoachRegistration},
-        {path:'/requests', component: RequestRecived},
-        {path:'/auth', component:AuthUsers},
+        {path:'/register', component: CoachRegistration, meta: {requiresAuth: true}},
+        {path:'/requests', component: RequestRecived, meta: {requiresAuth: true}},
+        {path:'/auth', component:AuthUsers, meta: {requiresUnauth: true}},
         {path:'/:notFound(.*)', component: NotFound},
     ],
+});
+
+router.beforeEach(function(to, _, next) {
+    if(to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next('/auth');
+    } else if(to.meta.requiresUnauth && store.getters.isAuthenticated) {
+        next('/coaches');
+    } else {
+        next();
+    }
 });
 
 export default router;
