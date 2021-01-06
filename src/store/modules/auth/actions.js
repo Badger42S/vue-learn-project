cb7030c5bb6c5ddc69a9668145def78a9e3e5145
Badger42S <57646489+Badger42S@users.noreply.github.com>
2021-01-06 09:04:00 -0500

    autoLogin

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,6 +1,24 @@
 export default{
     async login(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDKdynHm9uaj7zGr2fx9w2suU77WET9a0Y', 
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
+        });
+    },
+    async signup(context, payload) {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        });
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDKdynHm9uaj7zGr2fx9w2suU77WET9a0Y';
+        if(mode === 'signup') {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDKdynHm9uaj7zGr2fx9w2suU77WET9a0Y'
+        }
+
+        const response = await fetch(url, 
             {
                 method: 'POST',
                 body: JSON.stringify({
@@ -12,10 +30,12 @@ export default{
         const respData = await response.json();
 
         if(!response.ok) {
-            console.log(respData);
             const err = new Error(respData || 'Faild to authenticate');
             throw err;
         }
+
+        localStorage.setItem('token', respData.idToken);
+        localStorage.setItem('userId', respData.localId);
 
         context.commit('setUser', {
             token: respData.idToken,
@@ -23,29 +43,17 @@ export default{
             tokenExperation: respData.expiresIn
         });
     },
-    async signup(context, payload) {
-       const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDKdynHm9uaj7zGr2fx9w2suU77WET9a0Y', 
-            {
-                method: 'POST',
-                body: JSON.stringify({
-                    email: payload.email,
-                    password: payload.password,
-                    returnSecureToken: true
-                })
-        });
-        const respData = await response.json();
+    autoLogin(context) {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
 
-        if(!response.ok) {
-            console.log(respData);
-            const err = new Error(respData || 'Faild to authenticate');
-            throw err;
+        if(token && userId) {
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+                tokenExperation:null
+            });
         }
-
-        context.commit('setUser', {
-            token: respData.idToken,
-            userId: respData.localId,
-            tokenExperation: respData.expiresIn
-        });
     },
     logout(context) {
         context.commit('logout');
